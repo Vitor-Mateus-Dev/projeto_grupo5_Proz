46f5b44325a864cf7188479d9ad2712499886677
VitorMat <vitormateus891@outlook.com>
2023-06-04 11:58:42 -0300

    grande att Vitor

--- Sprint 3 - HTML e CSS/Js/faça_parte.js
+++ Sprint 3 - HTML e CSS/Js/faça_parte.js
@@ -1,30 +1,37 @@
+// chamando valores gerais
+const apareça1 = document.getElementById("apareça1")
+const login_container = document.getElementById("login-container")
+const section2 = document.getElementById("parte-2")
+const img = document.getElementById('image-frente')
+var mensagemBoasVindas = document.getElementById('mensagemBoasVindas');
+const logado = document.getElementById('logado')
+
 function registrarUsuario() {
-  // Obter os valores dos campos
+  // Obter os valores especificos registro
   const nomeSocial = document.getElementById('nomeSocial').value;
   const email = document.getElementById('email').value;
   const senha = document.getElementById('senha').value;
   const nomeCompleto = document.getElementById("nomeCompleto").value
-  const cpf = document.getElementById('cpf').value;
-  const logado = document.getElementById('logado')
-  const img = document.getElementById('image-frente')
-  const section2 = document.getElementById("parte-2")
-  var mensagemBoasVindas = document.getElementById('mensagemBoasVindas');
-  const apareça1 = document.getElementById("apareça1")
-  const login_container = document.getElementById("login-container")
-
-  // Validação dos campos (pode adicionar validações adicionais)
+  const cpf = document.getElementById('cpf').value.trim().replace(/[.-]/g, '');
+  
+  // Validação dos campos -------------------------------
   if (!cpf || !email || !senha || !nomeCompleto) {
     alert('Por favor, preencha todos os campos!');
     return;
   }
 
-  // Validar o formato do e-mail
+  // Validar o formato do e-mail -------------------------
   if (email.indexOf('@') === -1 || !email.endsWith('.com')) {
     alert('Por favor, insira um endereço de e-mail válido!');
     return;
   }
 
-  // Simulação do registro - Aqui você pode enviar os dados para um servidor ou armazená-los localmente
+  // Validar o formato do CPF ----------------------------
+  if (cpf.length !== 11) {
+    alert('Por favor, insira um CPF válido!');
+    return;
+  }
+
 
   // Exibir o conteúdo para o usuário logado e ocultar os formulários de registro e login
 
@@ -38,11 +45,11 @@ function registrarUsuario() {
 }
 
 function fazerLogin() {
-  // Obter os valores dos campos
+  // Obter os valores especificos
   const loginEmail = document.getElementById('loginEmail').value;
   const loginSenha = document.getElementById('loginSenha').value;
 
-  // Validação dos campos (pode adicionar validações adicionais)
+  // Validação dos campos
   if (!loginEmail || !loginSenha) {
     alert('Por favor, preencha todos os campos!');
     return;
@@ -54,7 +61,7 @@ function fazerLogin() {
     return;
   }
 
-  // Simulação do login - Aqui você pode verificar as credenciais em um servidor ou localmente
+
 
   // Exibir o conteúdo para o usuário logado e ocultar os formulários de registro e login
   logado.classList.add('d-none');
@@ -66,16 +73,15 @@ function fazerLogin() {
   mensagemBoasVindas.classList.remove = 'd-none';
 
 }
-// Botão ir para login e ir para registro com animação.
 
+// Botão ir para login e ir para registro com animação.
 const botaologin = document.getElementById('botaologin');
 const botaoregistro = document.getElementById("botaoregistro");
-const loginForm = document.getElementById("login-container");
 const idform = document.getElementById("idform")
 var image = document.querySelector(".image");
 
 botaologin.addEventListener('click', () => {
-  loginForm.classList.remove('d-none');
+  login_container.classList.remove('d-none');
   idform.classList.add('d-none');
   if (image.classList.contains("image-left")) {
     // Mover a imagem para a metade direita
@@ -88,7 +94,7 @@ botaologin.addEventListener('click', () => {
   }
 });
 botaoregistro.addEventListener('click', () => {
-  loginForm.classList.add('d-none');
+  login_container.classList.add('d-none');
   idform.classList.remove('d-none');
   if (image.classList.contains("image-left")) {
     // Mover a imagem para a metade direita
@@ -101,4 +107,42 @@ botaoregistro.addEventListener('click', () => {
   }
 });
 
+// colorações de certo e errado --------------------------
 
+const testenome = document.getElementById("nomeCompleto");
+testenome.addEventListener("input", function () {
+  const valor = testenome.value.trim();
+  if (valor.length < 5) {
+    testenome.classList.add("error");
+  } else {
+    testenome.classList.add("valid");
+    testenome.classList.remove("error");
+  }
+});
+
+// email --------------------------
+const emailcor = document.getElementById("email");
+emailcor.addEventListener("input", function () {
+  const valore = emailcor.value.trim();
+  if (!valore.includes('@') || !valore.endsWith('.com')) {
+    emailcor.classList.add("error");
+    emailcor.classList.remove("valid");
+  } else {
+    emailcor.classList.remove("error");
+    emailcor.classList.add("valid");
+  }
+});
+
+// cpf --------------------------
+const cpfcor = document.getElementById("cpf");
+cpfcor.addEventListener("input", function () {
+  let valorcpf = cpfcor.value.trim();
+  valorcpf = valorcpf.replace(/[.-]/g, ''); // Remove os símbolos "." e "-"
+  if (valorcpf.length !== 11) {
+    cpfcor.classList.add("error");
+    cpfcor.classList.remove("valid");
+  } else {
+    cpfcor.classList.remove("error");
+    cpfcor.classList.add("valid");
+  }
+});
